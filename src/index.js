1fde0abee2281f98e9ff93e365ee1a1fd45bd34e
Shinya Murai <sinmurai@gmail.com>
2021-10-26 04:49:15 +0900

    modified the way to call styles.css

--- src/index.js
+++ src/index.js
@@ -1,36 +1,38 @@
+import "./styles.css";
+
 /**
  * アロー関数
  */
 
-const test = (num) => {
-  return num + 5;
-};
+// const test = (num) => {
+//   return num + 5;
+// };
 
-console.log(test(2));
+// console.log(test(2));
 
-const test2 = (str = "村井") => console.log(`こんにちは${str}`);
-test2();
+// const test2 = (str = "村井") => console.log(`こんにちは${str}`);
+// test2();
 
-const test3 = [1, 2, 3, 4, 5];
-test3.map((number, index) => console.log(`${index + 1}番目は${number}です`));
+// const test3 = [1, 2, 3, 4, 5];
+// test3.map((number, index) => console.log(`${index + 1}番目は${number}です`));
 
-const test3_filter = test3.filter((number) => {
-  return number % 2 === 1;
-});
+// const test3_filter = test3.filter((number) => {
+//   return number % 2 === 1;
+// });
 
-console.log(test3_filter);
+// console.log(test3_filter);
 
-const newArr = ["村井", "真也", "奏太"];
-const test_san = newArr.map((str) => {
-  if (str === "村井") {
-    return `${str}さん`;
-  } else {
-    return str;
-  }
-});
+// const newArr = ["村井", "真也", "奏太"];
+// const test_san = newArr.map((str) => {
+//   if (str === "村井") {
+//     return `${str}さん`;
+//   } else {
+//     return str;
+//   }
+// });
 
-console.log(test_san);
+// console.log(test_san);
 
-const number_test = 1300;
-const formatNum = typeof number_test === "number" ? "Yeee" : "Nooo";
-console.log(formatNum);
+// const number_test = 1300;
+// const formatNum = typeof number_test === "number" ? "Yeee" : "Nooo";
+// console.log(formatNum);
